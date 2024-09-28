bf62f814fb58c6749892ce0a1ce0f8c313d6e174
Patricio Rios <85771918+PatricRc@users.noreply.github.com>
2024-09-28 09:36:30 -0500

    Update MultiwebApp.py

--- MultiwebApp.py
+++ MultiwebApp.py
@@ -2,8 +2,15 @@ import streamlit as st
 
 # --- PAGE SETUP ---
 def show_about_page():
-    st.title("About Me")
-    st.write("This is the About Me page.")
+    st.title("Patricio Rios - Data Analyst Portfolio")
+    st.subheader("Senior Data Analyst, assisting enterprises by supporting data-driven decision-making.")
+    st.write("Experience & Qualifications")
+    st.markdown("""
+    - 7 Years experience extracting actionable insights from data
+    - Strong hands-on experience and knowledge in Python and Excel
+    - Good understanding of statistical principles and their respective applications
+    - Excellent team-player and displaying a strong sense of initiative on tasks
+    """)
 
 def show_project_1_page():
     st.title("Sales Dashboard")
@@ -13,12 +20,16 @@ def show_project_2_page():
     st.title("Chat Bot")
     st.write("This is the Chat Bot page.")
 
-
-# --- NAVIGATION SETUP ---
-page = st.sidebar.selectbox(
-    "Navigate",
-    ("About Me", "Sales Dashboard", "Chat Bot")
+# --- SIDEBAR SETUP ---
+#st.sidebar.image("https://path_to_your_logo_image.com", width=150)  # Add your logo
+st.sidebar.markdown("### Info")
+page = st.sidebar.radio(
+    "Select a page:",
+    ["About Me", "Sales Dashboard", "Chat Bot"]
 )
+
+st.sidebar.markdown("### Projects")
+st.sidebar.markdown("- Sales Dashboard\n- Chat Bot")
 
 # --- PAGE DISPLAY ---
 if page == "About Me":
@@ -30,3 +41,4 @@ elif page == "Chat Bot":
 
 # --- SHARED ON ALL PAGES ---
 st.sidebar.markdown("Made with ❤️ by [Patricio](https://www.linkedin.com/in/patriciorioscanepa/)")
+
